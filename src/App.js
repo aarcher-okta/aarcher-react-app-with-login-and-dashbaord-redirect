54e3a8b6985c8e78461eecce20b00cf96d38aea9
Alex Archer <alex.archer@okta.com>
2022-06-21 16:47:01 -0400

    initial commit: add dashboard redirect logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, withRouter } from 'react-router-dom';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import { LoginCallback, Security, SecureRoute } from '@okta/okta-react';
+import Home from './Home';
+import Profile from './Profile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const oktaAuth = new OktaAuth({
+  issuer: 'https://login.archfaktorciam.xyz/oauth2/default',
+  clientId: '0oa1e9uyd1iXw4AAM697',
+  redirectUri: window.location.origin + '/login/callback'
+});
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.restoreOriginalUri = async (_oktaAuth, originalUri) => {
+      // set route route to "dashbaord" after authentication and callback to login URI - /dashboard is just a route that holds an external href link to the URL for the okta dashboard
+      props.history.replace('/dashboard');
+      //original restorUI code...
+      // props.history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+    };
+  }
+
+  render() {
+    return (
+      <Security oktaAuth={oktaAuth} restoreOriginalUri={this.restoreOriginalUri}>
+        <Route path="/" exact={true} component={Home}/>
+        <Route path="/login/callback" component={LoginCallback}/>
+        <SecureRoute path="/profile" component={Profile}/>
+        {/* set the external path in the dashboard route which is added to the history in the restorOriginalUri function above*/}
+        <SecureRoute path="/dashboard" component={() => {
+          window.location.href = 'https://login.archfaktorciam.xyz';
+          return null;
+        }}/>
+      </Security>
+    );
+  }
 }
 
-export default App;
+const AppWithRouterAccess = withRouter(App);
+
+class RouterApp extends Component {
+  render() {
+    return (<Router><AppWithRouterAccess/></Router>);
+  }
+}
+
+export default RouterApp;
